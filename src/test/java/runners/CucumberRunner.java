18c433965400d3080bd4359e3ed149fcbadae3c8
Magda <magdarojs@gmail.com>
2021-12-02 23:27:11 -0500

    1st commit

--- src/test/java/runners/CucumberRunner.java
+++ src/test/java/runners/CucumberRunner.java
@@ -10,16 +10,16 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
       tags ="@booking",//"@flightStatus",
         features = "src/test/resources",
-        glue = "stepDefinitions"
-       // stepNotifications = true
-    // ,dryRun = true   // dryRun scans the resources folder and generates step definition snippets
+        glue = "stepDefinitions",
+       stepNotifications = true
+   ,dryRun = true   // dryRun scans the resources folder and generates step definition snippets
         // used for generating snippets quickly
         ,snippets = CucumberOptions.SnippetType.UNDERSCORE  // generates Camel Case Style Snippets
-        ,plugin = {
-        "pretty",  // displays more detailed output on the console
-        "html:target/cucumber-built-in-report.html", // generates built in html report at the given the path
-        "json:target/cucumber.json"
-}
+//        ,plugin = {
+//        "pretty",  // displays more detailed output on the console
+//        "html:target/cucumber-built-in-report.html", // generates built in html report at the given the path
+//        "json:target/cucumber.json"
+//}
 
 )
 
